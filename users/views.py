87d8073ba9ce235323298aa5aa22d8b1942144bc
jaebooker <jaesonbooker@gmail.com>
2020-03-03 14:02:48 -0800

    added user app

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, you're in the Project index")
+    return HttpResponse("Hello, you're in the User index")
--- x_coordinate/urls.py
+++ x_coordinate/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('projects/', include('projects.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
