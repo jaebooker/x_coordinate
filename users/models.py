a76e6de689090a1df4f544c6d9cfced7ffd71cb5
jaebooker <jaesonbooker@gmail.com>
2020-03-03 14:04:44 -0800

    user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    joined_on = model.DateTimeField('date created')
+    description = models.CharField(max_length=500)
+    skills = models.CharField(max_length=200)
+    # projects = models.ForeignKey(Project, on_delete=models.CASCADE)
