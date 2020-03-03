6695f8f32195c2377b271e21b86ce02a0d28bf94
jaebooker <jaesonbooker@gmail.com>
2020-03-03 15:53:27 -0800

    project template and index

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, you're in the Project index")
+    project_list = Project.objects.order_by('-created_on')[:20]
+    context = {'project_list': project_list}
+    return render(request, 'projects/index.html', context)
 
 def detail(request, project_id):
     return HttpResponse("You're looking at user %s." % project_id)
