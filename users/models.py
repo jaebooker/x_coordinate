30b3b7e994d34beb054ef7ab07490d9e1d039bb2
jaebooker <jaesonbooker@gmail.com>
2020-03-06 16:01:50 -0800

    added user form model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from projects.models import Project
+from django import forms
+
+class UserForm(forms.Form):
+    user_name = forms.CharField(max_length=20)
+    description = forms.CharField(max_length=500)
+    skills = models.CharField(max_length=200)
 
 class User(models.Model):
     joined_on = model.DateTimeField('date created')
