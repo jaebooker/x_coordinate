5fd2ba7bf8916d47941c2bb13857a3ed859b9270
jaebooker <jaesonbooker@gmail.com>
2020-03-06 17:39:12 -0800

    altering project model and adding views

--- projects/models.py
+++ projects/models.py
@@ -2,11 +2,28 @@ from django.db import models
 from users.models import User
 from django import forms
 
-class ProjectForm(forms.Form):
-    title= forms.CharField(max_length=20)
-    description = forms.CharField(max_length=5000)
-    number_of_roles = models.IntegerField(default=0)
 
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+
+# class ProjectForm(forms.Form):
+#     title= forms.CharField(max_length=20)
+#     description = forms.CharField(max_length=5000)
+#     number_of_roles = models.IntegerField(default=0)
+# class ContactView(FormView):
+#     form_class = ContactForm
+#     template_name = 'contact-us.html'
+#     success_url = reverse_lazy('contact-us')
+#
+#     def form_valid(self, form):
+#         self.send_mail(form.cleaned_data)
+#         return super(ContactView, self).form_valid(form)
+#
+#     def send_mail(self, valid_data):
+#         # Send mail logic
+#         print(valid_data)
+#         pass
 class Project(models.Model):
     created_on = model.DateTimeField('date created')
     updated_on = model.DateTimeField('date created')
--- projects/views.py
+++ projects/views.py
@@ -11,3 +11,15 @@ def index(request):
 def detail(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
     return render(request, 'projects/detail.html', {'project': project})
+
+class ProjectFormView(CreateView):
+  def get(self, request):
+      context = {'form': ProjectForm()}
+      return render(request, 'projects/new_project.html', context)
+
+  def post(self, request):
+      form = ProjectForm(request.POST)
+      if form.is_valid():
+          project = form.save()
+          return HttpResponseRedirect(reverse_lazy('projects:detail', args=[project.id]))
+      return render(request, 'projects/new_project.html', {'form': form})
